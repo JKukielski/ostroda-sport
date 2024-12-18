a6e3db2f08a07982e43774d51b0f1aa83b682891
JKukielski <jankukielski1@gmail.com>
2024-12-18 21:22:54 +0100

    Rendering plog post cards

--- src/components/BlogPosts.jsx
+++ src/components/BlogPosts.jsx
@@ -1,35 +1,10 @@
-import { formatDate, truncateText } from '@/lib/utils';
-import { urlFor } from '@/sanity/lib/image';
-import { PortableText } from 'next-sanity';
-import Image from 'next/image';
+import BlogPostCard from './BlogPostCard';
 
 export default async function BlogPosts({ filteredPosts }) {
   return (
-    <div className="w-full flex flex-col gap-4">
+    <div className="w-[90%] flex flex-col justify-start items-start mb-5 gap-6 md:w-[100%] md:flex-row md:flex-wrap md:px-4 lg:w-[78%] lg:px-0 lg:gap-8">
       {filteredPosts.map((post) => (
-        <div key={post._id} className="w-full flex flex-col items-center">
-          <div className="w-[90%] h-[250px] relative">
-            <Image
-              src={urlFor(post.mainImage).url()}
-              alt="image"
-              fill
-              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-              style={{ objectFit: 'cover' }}
-              className="shadow-sm"
-            />
-          </div>
-          <div className="w-[90%] flex flex-col items-start">
-            <h2>{post.title}</h2>
-            {post.publishedAt ? (
-              <p className="">{formatDate(post.publishedAt)}</p>
-            ) : null}
-            <div>
-              {post.category.map((category) => (
-                <p key={category.slug.current}>{category.title}</p>
-              ))}
-            </div>
-          </div>
-        </div>
+        <BlogPostCard post={post} key={post._id} />
       ))}
     </div>
   );
