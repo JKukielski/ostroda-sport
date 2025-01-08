c47726b95493ea3ebe846923542130ba91ca1520
JKukielski <jankukielski1@gmail.com>
2025-01-08 19:38:51 +0100

    added list of locations

--- src/app/trenuj-z-nami/page.jsx
+++ src/app/trenuj-z-nami/page.jsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 export default function page() {
   return (
     <div className="w-full flex flex-col justify-start items-center py-6">
@@ -41,6 +43,35 @@ export default function page() {
             </b>
             .
           </p>
+          <div className="w-full flex flex-col justify-center items-start mb-2">
+            <h2 className="font-anton mt-4 text-xl md:text-2xl xl:text-3xl mb-2">
+              Lokalizacje
+            </h2>
+            <p className="text-xs text-gray-400 xl:text-sm">
+              Kliknij na lokalizację aby otworzyć mapy Google
+            </p>
+          </div>
+
+          <Link
+            href="https://maps.app.goo.gl/XPs7LKmyh49SjLS17"
+            target="_blank"
+          >
+            <b>Parking ul. Zientary Malewskiej</b>
+          </Link>
+          <Link
+            href="https://maps.app.goo.gl/zSbWuiLi2AuAQpo28"
+            target="_blank"
+          >
+            <b>Stadion lekkoatletyczny</b>
+          </Link>
+          <Link
+            href="https://maps.app.goo.gl/L7YbAKhykCQ96EDM6"
+            target="_blank"
+          >
+            <b>
+              Hala Liceum Ogtólnokształące nr I im Jana Bażyńskiego w Ostródzie
+            </b>
+          </Link>
         </div>
       </div>
     </div>
